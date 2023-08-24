75a945b9fadf2f8a8648d7424c10ebab7cdb0638
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-08-24 08:55:33 -0300

    CRUD PHP

---
+++ fabricantes/visualizar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Visualização</title>
+</head>
+<body>
+    <a href="../index.php">Home</a>
+    <h1>Fabricantes | SELECT</h1>
+    <hr>
+    <h2>Lendo e carregando todos os fabricantes.</h2>
+    <p>
+        <a href="inserir.php">Inserir fabricantes</a>
+    </p>
+</body>
+</html>
