435b8027655fcaeff1e08683a9f17ad4c78b0632
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-08-25 09:14:07 -0300

    bindValue()

--- src/funcoes_fabricantes.php
+++ src/funcoes_fabricantes.php
@@ -28,6 +28,23 @@ function lerFabricantes(PDO $conexao){
 
 
     return $resultado;
+} 
+
+
+// Usada em fabricantes/inserir.php
+function inserirFabricante(PDO $conexao, string $nomeDoFabricante){
+    // :qualquerCoisa -> isso indica um "named parameter" (parâmetro nomeado)
+    $sql = "INSERT INTO fabricantes(nome) VALUE(:nome)";
+
+    try {
+        $consulta = $conexao->prepare($sql);
+
+        // bindValue() -> permite vincular o valor existente no parâmetro nomeado (:nome) à consulta que será executada. É necessário indicar qual é o parâmetro (:nome), de onde vem o valor ($nomeDoFabricante) e de  que tipo ele é (PDO::PARAM_STR) 
+        $consulta->bindValue(":nome", $nomeDoFabricante, PDO::PARAM_STR);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao inserir: ".$erro->getMessage());
+    }
 }
 
 
